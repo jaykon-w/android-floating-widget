ec2fb614fda0e743317a9beceaf382877df870fd
Jaykon Willian de Oliveira <joliveira@rastreabilidadebrasil.com.br>
2021-06-28 10:13:56 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### android-floating-widget
+Android Floating Widget Service
+
+### Screenshots
+<img src="https://github.com/hu2di/android-floating-widget/blob/master/Screenshots/Screenshot_20170328-175503.png" height="720">
+<img src="https://github.com/hu2di/android-floating-widget/blob/master/Screenshots/Screenshot_20170328-175508.png" height="720">
+
+### About
+- Created by HuyHung Dinh
+- GitHub: https://github.com/hu2di
+- Email: hebitaxy@gmail.com
---
+++ app/src/main/java/com/gpaddy/hungdh/floatingwidget/FloatingViewService.java
@@ -0,0 +1,157 @@
+package com.gpaddy.hungdh.floatingwidget;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.opengl.Visibility;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class FloatingViewService extends Service {
+
+    private WindowManager mWindowManager;
+    private View mFloatingView;
+
+    public FloatingViewService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    boolean goneFlag = false;
+
+    //Put this into the class
+    final Handler handler = new Handler();
+    Runnable mLongPressed;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Inflate the floating view layout we created
+        mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null);
+
+        //Add the view to the window.
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        //Specify the view position
+        params.gravity = Gravity.TOP | Gravity.LEFT;        //Initially view will be added to top-left corner
+        params.x = 0;
+        params.y = 100;
+
+        //Add the view to the window
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mWindowManager.addView(mFloatingView, params);
+
+        //The root element of the collapsed view layout
+        final View collapsedView = mFloatingView.findViewById(R.id.collapse_view);
+        final View collapsedIVView = mFloatingView.findViewById(R.id.collapsed_iv);
+
+        //Set the close button
+        ImageView closeButtonCollapsed = (ImageView) mFloatingView.findViewById(R.id.close_btn);
+        closeButtonCollapsed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //close the service and remove the from from the window
+                stopSelf();
+            }
+        });
+
+        mLongPressed = new Runnable() {
+            public void run() {
+                goneFlag = !goneFlag;
+                collapsedIVView.setVisibility(goneFlag ? View.VISIBLE: View.GONE);
+            }
+        };
+
+        //Drag and move floating view using user's touch action.
+        mFloatingView.findViewById(R.id.root_container).setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        handler.postDelayed(mLongPressed, 2000);
+                        if(goneFlag) return true;
+
+                        //remember the initial position.
+                        initialX = params.x;
+                        initialY = params.y;
+
+                        //get the touch location
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        handler.removeCallbacks(mLongPressed);
+                        if(goneFlag) return true;
+                        int Xdiff = (int) (event.getRawX() - initialTouchX);
+                        int Ydiff = (int) (event.getRawY() - initialTouchY);
+
+
+                        //The check for Xdiff <10 && YDiff< 10 because sometime elements moves a little while clicking.
+                        //So that is click event.
+                        if (Xdiff < 10 && Ydiff < 10) {
+                            if (isViewCollapsed()) {
+                                //When user clicks on the image view of the collapsed layout,
+                                //visibility of the collapsed layout will be changed to "View.GONE"
+                                //and expanded view will become visible.
+                                collapsedView.setVisibility(View.VISIBLE);
+                            }
+                        }
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        if(goneFlag) return true;
+                        //Calculate the X and Y coordinates of the view.
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+
+                        if (Math.abs(params.x - initialX) > 5 && Math.abs(params.y - initialY) > 5) {
+                            handler.removeCallbacks(mLongPressed);
+                        }
+
+
+                        //Update the layout with new X & Y coordinate
+                        mWindowManager.updateViewLayout(mFloatingView, params);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
+    /**
+     * Detect if the floating view is collapsed or expanded.
+     *
+     * @return true if the floating view is collapsed.
+     */
+    private boolean isViewCollapsed() {
+        return mFloatingView == null || mFloatingView.findViewById(R.id.collapse_view).getVisibility() == View.VISIBLE;
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mFloatingView != null) mWindowManager.removeView(mFloatingView);
+    }
+}
